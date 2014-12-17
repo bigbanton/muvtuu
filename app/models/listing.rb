4c4c5dd3ad116590170f4fc859b006e490402cbf
Craig Banton <c.banton12@gmail.com>
2014-12-17 11:39:27 -0500

    Add validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -11,4 +11,6 @@ has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }
 end
 validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png),
 :path => ":style/:id_:filename"
+validates :name, :price, :description, presence: true
+validates :price, numericality: { greater_than: 0}
 end
